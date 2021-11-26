99053f4abbf368b0b30d078955402bd439d7619b
Arkadii Zuev <arkasha.zuev@icloud.com>
2021-11-26 21:23:48 +0500

    Initial Commit

---
+++ ParallaxMovies/ParallaxMoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParallaxMoviesApp.swift
+//  ParallaxMovies
+//
+//  Created by Arkasha Zuev on 26.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct ParallaxMoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
